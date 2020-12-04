ff62d0b7c01f36b40fa11ad76064079b25b1ebfc
Frank <fchavez@armalot.com.ve>
2020-12-04 16:42:34 -0400

    Funciona el soap!

--- src/Controller/BilleteraController.php
+++ src/Controller/BilleteraController.php
@@ -2,21 +2,44 @@
 
 namespace App\Controller;
 
+use SoapClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BilleteraController extends AbstractController
 {
+    private $soap;
+
+    public function __construct()
+    {
+        $opts = array(
+            'trace' => 1,
+            'uri' => '/billetera-movil/public/index.php/soap',
+            'location' => 'http://localhost/billetera-movil/public/index.php/soap'
+        );
+        $this->soap = new SoapClient(NULL, $opts);
+    }
+
     /**
-     * @Route("/", name="user_list")
+     * @Route("/", name="Billetera_list")
      * 
      * @return void
      */
-    public function list()
+    public function list(Request $request)
     {
         $response = new Response();
-        $response->setContent('<div><h1>Hola Mundo</h1></div>');
-        return $response;
+        return $response->setContent($this->generateUrl('Soap_index'));
+        // return $this->redirectToRoute('Billetera_soapclient');
+    }
+
+    /**
+     * @Route("/soapclient", name="Billetera_soapclient")
+     */
+    public function soapClient()
+    {
+        $data = ['name' => 'Frank'];
+        return (new Response())->setContent($this->soap->__soapCall('hello', array($data)));
     }
 }
---
+++ src/Controller/SoapController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\SoapService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SoapController extends AbstractController
+{
+    /**
+     * @Route("/soap", name="Soap_index")
+     */
+    public function index(SoapService $soapService)
+    {
+        $soapServer = new \SoapServer(NULL, ['uri' => '/billetera-movil/public/index.php/soap']);
+        $soapServer->setObject($soapService);
+        $soapServer->handle();
+    }
+}
